c8da03b0a228759c33de34770c382e426d350a62
Eran Zimbler <eran.zimbler@avg.com>
2014-10-29 09:06:29 +0200

    removing flags from get command

--- get.go
+++ get.go
@@ -5,22 +5,18 @@ import (
 	"fmt"
 	"github.com/BurntSushi/toml"
 	"net/http"
+	"strings"
 )
 
 var cmdGet = &Command{
-	UsageLine: "get [-p] [-r]",
+	UsageLine: "get <path to project definition file>",
 	Short:     "create the directory structure of a project according to project definition file",
 	Long: `
 The get command parse the project definition file and downloads the relevant sources into current directory.
 Running without any flag will search for a file called projectdef.toml in the current directory.
-The -p flag allows to specify a file to work on.
-The -r flag allows to specify a remote file (from a git repository)
 	`,
-	CustomFlags: true,
+	CustomFlags: false,
 }
-
-var getP = cmdGet.Flag.String("p", "projectdef.toml", "Specify project definition file")
-var getR = cmdGet.Flag.String("r", "", "remote url for project definition file")
 
 func init() {
 	cmdGet.Run = runGet // break init loop
@@ -32,28 +28,38 @@ func create(s source, ch chan bool) {
 	ch <- true
 }
 
-func runGet(cmd *Command, args []string) {
+func proccessConfig(path string) (Config, error) {
 	var config Config
-	if *getR != "" {
+	if strings.HasPrefix(strings.ToLower(path), "http") {
 		fmt.Println("working with remote definition file")
 		tr := &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		}
 		client := &http.Client{Transport: tr}
-		res, _ := client.Get(*getR)
+		res, _ := client.Get(path)
 
-		if _, err := toml.DecodeReader(res.Body, &config); err != nil {
+		_, err := toml.DecodeReader(res.Body, &config)
+		if err != nil {
 			fmt.Println(err)
-			return
+			return config, err
 		}
 		res.Body.Close()
-	} else {
-		if _, err := toml.DecodeFile(*getP, &config); err != nil {
-			fmt.Println(err)
-			return
-		}
+		return config, nil
 	}
+	_, err := toml.DecodeFile(path, &config)
+	if err != nil {
+		fmt.Println(err)
+		return config, err
+	}
+	return config, nil
+}
 
+func runGet(cmd *Command, args []string) {
+
+	config, err := proccessConfig(args[0])
+	if err != nil {
+		return
+	}
 	done := make(chan bool, len(config.Sources))
 	for _, source := range config.Sources {
 		source.set_type()
