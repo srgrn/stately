15a632d261e0d3a0bf46670a891f815356f2da08
Eran Zimbler <eran.zimbler@avg.com>
2014-10-30 18:24:04 +0200

    using another file for proccessConfig

--- get.go
+++ get.go
@@ -28,32 +28,6 @@ func create(s source, ch chan bool) {
 	ch <- true
 }
 
-func proccessConfig(path string) (Config, error) {
-	var config Config
-	if strings.HasPrefix(strings.ToLower(path), "http") {
-		fmt.Println("working with remote definition file")
-		tr := &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
-		client := &http.Client{Transport: tr}
-		res, _ := client.Get(path)
-
-		_, err := toml.DecodeReader(res.Body, &config)
-		if err != nil {
-			fmt.Println(err)
-			return config, err
-		}
-		res.Body.Close()
-		return config, nil
-	}
-	_, err := toml.DecodeFile(path, &config)
-	if err != nil {
-		fmt.Println(err)
-		return config, err
-	}
-	return config, nil
-}
-
 func runGet(cmd *Command, args []string) {
 
 	config, err := proccessConfig(args[0])
